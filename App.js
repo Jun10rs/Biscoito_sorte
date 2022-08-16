08860ba66004bb922cbba30826d3f960949512a1
Junior Silva <osnildo_silva-junior@estudante.sc.senai.br>
2022-08-15 22:28:22 -0300

    att app

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  SafeAreaView,
+  Image,
+  ImageBackground,
+  StyleSheet,
+  StatusBar,
+  Button,
+} from "react-native";
+
+import background from "./assets/fundo.png";
+import biscoito from "./assets/biscuit.png";
 
 export default function App() {
+
+  const minhasFrases = [
+    'Olá pequeno Padawan - A vida trará coisas boas se tiver paciência', 
+    'Olá pequeno Padawan - Não compense na ira o que lhe falta na razão', 
+    'Olá pequeno Padawan - A maior de todas as torres começa no solo', 
+    'Olá pequeno Padawan - Siga os bons e aprenda com eles',
+    'Olá pequeno Padawan - A sorte favorece a mente bem preparada',
+    'Olá pequeno Padawan - Você é do tamanho do seu sonho',
+    'Olá pequeno Padawan - Nós somos o que pensamos',
+    'Olá pequeno Padawan - A vontade das pessoas é a melhor das leis',
+    'Olá pequeno Padawan - Acredite em milagres, mas não dependa deles',
+    'Olá pequeno Padawan - Você sempre será a sua melhor companhia!',
+]
+  
+  function quebrarBiscoito() {
+    const numeroAleatorio = Math.random() * 10
+    const numeroInteiro = Math.floor(numeroAleatorio)
+
+    alert (minhasFrases[numeroInteiro])
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <ImageBackground source={background} style={styles.imageApp}>
+        <StatusBar backgroundColor="#e8ad86" />
+
+        <Image source={biscoito} style={styles.logo} />
+        <Button
+          title="Quebrar biscoito"
+          color="#A52A2A"
+          onPress={quebrarBiscoito}
+        />
+
+      </ImageBackground>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  imageApp: {
+    width: "100%",
+    height: "100%",
+    alignItems:'center',
+    justifyContent: 'center'
+  },
+  logo: {
+    width: 150,
+    height: 150,
   },
 });
+
